27dae242b5f54bb9b1f1a36ed2db29aec90ff194
Tyler Long <aethdae@gmail.com>
2026-02-16 20:50:58 -0600

    add extra properties

--- app.js
+++ app.js
@@ -3,13 +3,24 @@ let score = 0;
 const isActive = true;
 const tags = ["js", "week3"];
 
-const user = { id: 1, favoriteColor: "blue", trailingComma: false };
+const user = {
+  id: 1,
+  favoriteColor: "blue",
+  trailingComma: false,
+  extra: {
+    base: null,
+    newContent: null,
+    lastName: "Long",
+  },
+};
 
 console.log(user["favoriteColor"]);
 
 user["favoriteColor"] = "black";
 
-console.log(user["favoriteColor"]);
+console.log(user.favoriteColor);
+
+console.log(user.extra.lastName);
 
 function add(a, b) {
   return a + b;
